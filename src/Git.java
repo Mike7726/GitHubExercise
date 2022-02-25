28695eb003e8288bf1798f89d3a95773be4cf7b0
Mike7726 <Mike7726@stud.kea.dk>
2022-02-25 11:12:27 +0100

    test commit for at se om den bliver pushed

--- src/Git.java
+++ src/Git.java
@@ -8,11 +8,12 @@ public class Git {
     Scanner scanner = new Scanner(System.in);
     System.out.print("Type your name: ");
     String userInput = scanner.nextLine();
-
     System.out.print("Type your age: ");
     int userInput2 = scanner.nextInt();
 
     System.out.printf("\nWelcome, " + userInput + "(%d)\n", userInput2);
+
+    System.out.println("Test");
 
   }
 }
