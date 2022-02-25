784d68316a66a5d3d916c03c8bd17bec809fa67d
Mike7726 <Mike7726@stud.kea.dk>
2022-02-25 09:14:09 +0100

    tilføjede scanner + sout med userinput

--- src/Git.java
+++ src/Git.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Git {
   public static void main(String[] args) {
     System.out.println("It works!");
+    System.out.println("It still works");
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Type your name: ");
+    String userInput = scanner.nextLine();
+
+
   }
 }
