26a1581b0306f26dee5c428b444101434f91f36b
Mike7726 <Mike7726@stud.kea.dk>
2022-02-25 09:45:44 +0100

    tilføjede linje afstand i userOutput

--- src/Git.java
+++ src/Git.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Git {
   public static void main(String[] args) {
     System.out.println("It works!");
-    System.out.println("It still works");
+    System.out.println("It still works\n");
 
     Scanner scanner = new Scanner(System.in);
     System.out.print("Type your name: ");
@@ -12,7 +12,7 @@ public class Git {
     System.out.print("Type your age: ");
     int userInput2 = scanner.nextInt();
 
-    System.out.printf("Velkommen, " + userInput + "(%d)", userInput2);
+    System.out.printf("\nWelcome, " + userInput + "(%d)\n", userInput2);
 
   }
 }
